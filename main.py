8390dbdd802355f1212fb0336b18c1dd08ce3740
chiefstyn <aubergz@gmail.com>
2024-12-27 20:16:28 -0800

    added deta time game clock limit

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import pygame
 def main():
 
         pygame.init()
+        
+        game_clock = pygame.time.Clock() 
+        dt = 0
+
         print("Starting asteroids!")
         print("Screen width: 1280")
         print("Screen height: 720")
@@ -16,6 +20,10 @@ def main():
                                 return
                 screen.fill((0, 0, 0))
                 pygame.display.flip()
+                dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
+
+
+
