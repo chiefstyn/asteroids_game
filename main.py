2e43dd0c49a49c80035a6097cd216d5c88410aa4
chiefstyn <aubergz@gmail.com>
2024-12-27 19:55:05 -0800

    cleaned up code

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import pygame
+
 def main():
+
+        pygame.init()
         print("Starting asteroids!")
         print("Screen width: 1280")
         print("Screen height: 720")
-pygame.init
-from constants import *
-screen = pygame.display.set_mode((1280, 720))
 
-if __name__ == "__main__":
-    main()
+        screen = pygame.display.set_mode((1280, 720))
+        
 
-def main():
         while True:
                 for event in pygame.event.get():
                         if event.type == pygame.QUIT:
@@ -18,5 +17,5 @@ def main():
                 screen.fill((0, 0, 0))
                 pygame.display.flip()
 
-main()
-        
+if __name__ == "__main__":
+    main()
